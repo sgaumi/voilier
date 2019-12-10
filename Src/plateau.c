6ac1b972295006b9238498aeeb67cc4446eaddbc
Simeon Gaumart <gaumart@insat.insa-toulouse.fr>
2019-12-10 17:03:26 +0100

    fin projet

--- Src/plateau.c
+++ Src/plateau.c
@@ -26,7 +26,7 @@ void timer_RF_init(){
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM4);
 	
 	LL_TIM_InitTypeDef timer_init;
-	timer_init.Autoreload=65530; 
+	timer_init.Autoreload=19999; 
 	timer_init.Prescaler=71; //pour avoir 1 000 000 Tick/s (72 000 000/(psc+1))
 	timer_init.ClockDivision=LL_TIM_CLOCKDIVISION_DIV1;
 	timer_init.CounterMode=LL_TIM_COUNTERMODE_UP;
@@ -71,8 +71,9 @@ void get_vitesse_sens(){
 	int pulse = LL_TIM_OC_GetCompareCH2(TIM4) +1;
 	int period = LL_TIM_OC_GetCompareCH1(TIM4) +1;
 	
-	vitesse = ((float)pulse/1000.)*2. -3.; //vitesse entre -1 et 1
-	
+	pulse=pulse;
+	//vitesse = ((float)(pulse)/1000.)*2. -3.; //vitesse entre -1 et 1
+	vitesse=-0.5;	
 	if (vitesse>=0.){
 		sens=1;
 	}
@@ -103,7 +104,7 @@ void gpio_mcc_init() {
 	//sens PA2 tim 2 ch3
 	LL_GPIO_InitTypeDef pa2;
 	pa2.Pin = LL_GPIO_PIN_2;
-	pa2.Mode = LL_GPIO_MODE_INPUT;
+	pa2.Mode = LL_GPIO_MODE_OUTPUT;
 	pa2.Speed = LL_GPIO_MODE_OUTPUT_10MHz;
 	pa2.OutputType = LL_GPIO_OUTPUT_PUSHPULL ;
 	pa2.Pull = LL_GPIO_PULL_UP ;
@@ -128,38 +129,39 @@ void timer_pwm_mcc_init() {
 	Timer.CounterMode=LL_TIM_COUNTERMODE_UP;
 	Timer.RepetitionCounter=0;
 		
-	LL_TIM_Init(TIM4,&Timer);
+	LL_TIM_Init(TIM2,&Timer);
 	
-	LL_TIM_OC_SetMode (TIM2, LL_TIM_CHANNEL_CH1,LL_TIM_OCMODE_PWM1);
+	LL_TIM_OC_SetMode (TIM2, LL_TIM_CHANNEL_CH2,LL_TIM_OCMODE_PWM1);
 
-	//rajouter les deux lignes des flags
-	TIM2->BDTR |= TIM_BDTR_MOE;
-	TIM2->CCER |= TIM_CCER_CC1E;
+
+	TIM2->CCER |= TIM_CCER_CC2E;
 	
 	LL_TIM_EnableCounter(TIM2);
 	
 }	
 	
-	
+int i=0;
 	
 void bougerPlateau() {
+	
+	float vitesse2;
+	int vit2;
 
 	if (sens) { 	//peut-être dans l'autre sens
-		GPIOA -> ODR |= 0x00 ; 
+		GPIOA -> ODR &= ~0x00000004 ; 
 	}
 	else
 	{
-		GPIOA -> ODR |= 0x01 ;
+		GPIOA -> ODR |= 0x00000004 ;
 	}
 	
-	vitesse=abso(vitesse); // on recoit une vitesse entre -1 et 1 et on fait un seuil entre -0.2 et 0.2 où c'est stable
-	if (vitesse<0.2) {
-		vitesse = 0;
+	vitesse2=abso(vitesse); // on recoit une vitesse entre -1 et 1 et on fait un seuil entre -0.2 et 0.2 où c'est stable
+	if (vitesse2<0.2) {
+		vitesse2 = 0.0;
 	}
 	else {
-		 vitesse=(vitesse-0.2)/0.8 ;
+		 vitesse2=((vitesse2-0.2)/0.8)*65454.0 ;
 	}
-	
-	LL_TIM_OC_SetCompareCH1(TIM2, vitesse*65454); //vitesse à convertir en pourcentage     Arr=65454
-	
+	vit2 = (int)(vitesse2) ;
+	LL_TIM_OC_SetCompareCH2(TIM2, vit2); //vitesse à convertir en pourcentage     Arr=65454 //2339162.5
 }
